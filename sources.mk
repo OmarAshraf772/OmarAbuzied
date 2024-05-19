3a98ea9b99322d119107ddb82e1ef6cf648192b6
OmarAshraf772 <64014848+OmarAshraf772@users.noreply.github.com>
2024-05-19 15:00:01 +0300

    Add files via upload

---
+++ sources.mk
@@ -0,0 +1,38 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+ELFS := 
+OBJS := 
+ASM_DEPS := 
+S_DEPS := 
+SIZEDUMMY := 
+S_UPPER_DEPS := 
+LSS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+COTS/APP \
+COTS/HAL/AT24C08 \
+COTS/HAL/KPD \
+COTS/HAL/LCD \
+COTS/MCAL/ADC \
+COTS/MCAL/DIO \
+COTS/MCAL/EXTI \
+COTS/MCAL/GIE \
+COTS/MCAL/ICU \
+COTS/MCAL/SPI \
+COTS/MCAL/TIMER0 \
+COTS/MCAL/TIMER1 \
+COTS/MCAL/TWI \
+COTS/MCAL/USART \
+COTS/MCAL/WDT \
+COTS/MCAL/timer \
+
